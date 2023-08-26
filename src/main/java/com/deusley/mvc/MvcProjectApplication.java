3ecb543ab397f5ebcc143792361b31f3b60688aa
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-08-26 02:55:40 -0300

    firtCommit

---
+++ src/main/java/com/deusley/mvc/MvcProjectApplication.java
@@ -0,0 +1,16 @@
+package com.deusley.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class MvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcProjectApplication.class, args);
+	}
+
+}
