11b95a2e69671a1ae9fcc103f174e65880f73c31
Deuslleyw <Deuslleyw@hotmail.com>
2024-02-13 02:22:47 -0300

    validUserFileds

--- src/main/java/com/deusley/mvc/domain/Endereco.java
+++ src/main/java/com/deusley/mvc/domain/Endereco.java
@@ -2,30 +2,46 @@ package com.deusley.mvc.domain;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "ENDERECOS")
 public class Endereco extends AbstractEntity<Long> {
 
+    @NotBlank
+    @Size(min = 3, max = 255)
     @Column(nullable = false)
     private String logradouro;
 
+    @NotBlank
+    @Size(min = 3, max = 255)
     @Column(nullable = false)
     private String bairro;
 
+    @NotBlank
+    @Size(min = 3, max = 255)
     @Column(nullable = false)
     private String cidade;
 
+    @NotNull(message = "{NotNul.endereco.uf}")
     @Column(nullable = false, length = 2)
     @Enumerated(EnumType.STRING)
     private UF uf;
 
+    @NotBlank
+    @Size(min = 3, max = 9, message = "{Size.endereco.cep}")
     @Column(nullable = false, length = 9)
     private String cep;
 
+    @NotNull(message = "{NotNul.endereco.numero}")
+    @Digits(integer = 5, fraction = 0)
     @Column(nullable = false, length = 5)
     private Integer numero;
 
+    @Size(max = 100)
     private String complemento;
 
     public String getLogradouro() {
--- src/main/java/com/deusley/mvc/webController/FuncionarioController.java
+++ src/main/java/com/deusley/mvc/webController/FuncionarioController.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -38,7 +40,12 @@ public class FuncionarioController {
     }
 
     @PostMapping("/salvar")
-    public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+    public String salvar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr) {
+
+        if (result.hasErrors()){
+            return "/funcionario/cadastro";
+        }
+
         service.salvar(funcionario);
         attr.addFlashAttribute("success", "Funcionario inserido com sucesso");
         return "redirect:/funcionarios/cadastrar";
@@ -62,7 +69,12 @@ public class FuncionarioController {
     }
 
     @PostMapping("/editar")
-    public String editar(Funcionario funcionario, RedirectAttributes attr) {
+    public String editar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr) {
+
+        if (result.hasErrors()){
+            return "/funcionario/cadastro";
+        }
+
         service.editar(funcionario);
         attr.addFlashAttribute("success", "Editado com sucesso");
         return "redirect:/funcionarios/cadastrar";
